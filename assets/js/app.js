2caddc566e8726d5ddf73786da7f06c7af32dfff
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-06-21 13:59:17 +0530

    section-5 completed with responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,29 @@ let menulist = document.querySelector(".navbar-iteams");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
 })
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
